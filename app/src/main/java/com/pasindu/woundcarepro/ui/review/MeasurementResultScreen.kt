1144d05d63e52f78f9eec5934b245b7e0aa365f9
pasindu93619 <41615979+pasindu93619@users.noreply.github.com>
2026-02-18 05:26:04 +0530

    Fix Compose KeyboardOptions imports in review screens

--- app/src/main/java/com/pasindu/woundcarepro/ui/review/MeasurementResultScreen.kt
+++ app/src/main/java/com/pasindu/woundcarepro/ui/review/MeasurementResultScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -18,7 +19,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.pasindu.woundcarepro.data.local.AssessmentDao
