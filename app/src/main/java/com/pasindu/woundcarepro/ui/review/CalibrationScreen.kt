cd687b3afb32de2decf7eeb3432b87b66873d895
pasindu <pasindu93pavithra@gmail.com>
2026-02-20 23:53:58 +0530

    Fix CalibrationScreen weight import

--- app/src/main/java/com/pasindu/woundcarepro/ui/review/CalibrationScreen.kt
+++ app/src/main/java/com/pasindu/woundcarepro/ui/review/CalibrationScreen.kt
@@ -6,13 +6,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
