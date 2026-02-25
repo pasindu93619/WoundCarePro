a06ce349f6513552a58459f73ecb6210662fc113
pasindu <pasindu93pavithra@gmail.com>
2026-02-25 07:32:12 +0530

    Add TFLite dependencies for AI segmentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.foundation:foundation")
+    implementation("org.tensorflow:tensorflow-lite:2.14.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
 
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
